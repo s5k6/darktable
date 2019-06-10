63b779d37978f936ceca63d964fefbdcbe9171f7
Stefan Klinger <git@stefan-klinger.de>
2019-06-10 14:32:43 +0200

    Remove unused modules when compressing history.

--- src/libs/history.c
+++ src/libs/history.c
@@ -715,6 +715,15 @@ static void _lib_history_compress_clicked_callback(GtkWidget *widget, gpointer u
   sqlite3_step(stmt);
   sqlite3_finalize(stmt);
 
+  // remove switched-off modules
+  DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
+    "DELETE FROM main.history WHERE imgid = ?1 AND enabled == 0",
+    -1, &stmt, NULL);
+  DT_DEBUG_SQLITE3_BIND_INT(stmt, 1, imgid);
+  sqlite3_step(stmt);
+  sqlite3_finalize(stmt);
+
+
   // delete all mask_manager entries
   int masks_count = 0;
   char op_mask_manager[20] = {0};
